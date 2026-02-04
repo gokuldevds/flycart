54f4d6e7db0de2d1e39e150db201d83d5d7aa623
Gokul Dev D S <gokuldevds@gmail.com>
2026-02-04 14:46:12 +0530

    Add root route to fix 404 on health check

--- server/index.js
+++ server/index.js
@@ -12,6 +12,11 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// Basic Route
+app.get('/', (req, res) => {
+    res.send('FlyCart API is running...');
+});
+
 // Database Connection
 mongoose.connect(process.env.MONGO_URI)
     .then(() => console.log('MongoDB Connected'))
